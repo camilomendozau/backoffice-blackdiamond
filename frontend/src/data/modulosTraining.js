18e55f591b99d6e3850ab01fc58709f74eecfd6c
Camilo Mendoza <camilomen@localhost.localdomain>
2026-07-03 10:34:02 -0400

    Fix: Video import for cloudinary and capacitacion  modules

--- frontend/src/data/modulosTraining.js
+++ frontend/src/data/modulosTraining.js
@@ -1,17 +1,17 @@
-import module1 from "../assets/training/module1.mp4";
-import module2 from "https://res.cloudinary.com/dbnf8c8jf/video/upload/v1780706844/module2_tfqogt.mp4";
+//import module1 from "../assets/training/module1.mp4";
+// import module2 from "https://res.cloudinary.com/dbnf8c8jf/video/upload/v1780706844/module2_tfqogt.mp4";
 export const modulos = [
   {
     id: 1,
     title: "Módulo 1 - Introducción al Sistema de Formación",
     descripcion: "En este modulo aprenderás como funciona la industria de redes de mercadeo y por que trabajar en la industria.",
-    src: module1,
+    src: "https://res.cloudinary.com/dbnf8c8jf/video/upload/v1780706844/module2_tfqogt.mp4",
     siguiente: 2,
   },
   {
     id: 2,
     title: "Módulo 2 - Sistema de Duplicación",
     descripcion: "En este modulo aprenderás como duplicar tu negocio.",
-    src: module2,
+    src: "https://res.cloudinary.com/dbnf8c8jf/video/upload/v1780706844/module2_tfqogt.mp4",
   },
 ];
