4d8f5e6ac983afd043af26553986d74750b107cd
Camilo Mendoza <camilomen@localhost.localdomain>
2026-07-21 18:03:45 -0400

    Fix: New module1 video link of cloudinary

--- frontend/src/data/modulosTraining.js
+++ frontend/src/data/modulosTraining.js
@@ -5,7 +5,7 @@ export const modulos = [
     id: 1,
     title: "Módulo 1 - Introducción al Sistema de Formación",
     descripcion: "En este modulo aprenderás como funciona la industria de redes de mercadeo y por que trabajar en la industria.",
-    src: "https://res.cloudinary.com/dbnf8c8jf/video/upload/v1780706844/module2_tfqogt.mp4",
+    src: "https://res.cloudinary.com/dbnf8c8jf/video/upload/v1784674750/module1_Q5oIDW6n_sgizcn.mp4",
     siguiente: 2,
   },
   {
